9d02e23a4eaefc4e1f15a321aba4b0d6d4cf835d
Szentkuti Adrian <szentkuti.adrian@gmail.com>
2019-08-13 15:12:41 +0200

    2800points

--- data_handler.py
+++ data_handler.py
@@ -1,1 +1,37 @@
-asd
+import csv
+
+ANSWER_PATH = "data/answer.csv"
+QUESTION_PATH = "data/question.csv"
+
+def get_all_data(path):
+    rows = []
+    with open(path,"r") as f:
+        csv_reader = csv.DictReader(f, delimiter = ',',restval="")
+        for row in csv_reader:
+            row_dict = {}
+            for header in row:
+                row_dict[header] = row[header]
+            rows.append(row_dict)
+        return rows
+
+def get_q_by_id(qid):
+    with open(QUESTION_PATH,"r") as f:
+        csv_reader = csv.DictReader(f, delimiter = ',',restval="")
+        question_dict = {}
+        for row in csv_reader:
+            if row["id"] == qid:
+                for header in row:
+                    question_dict[header] = row[header]
+        return question_dict
+
+def get_answers_by_qid(qid):
+    answers = []
+    with open(ANSWER_PATH,"r") as f:
+        csv_reader = csv.DictReader(f, delimiter = ',',restval="")
+        for answer in csv_reader:
+            if answer["question_id"] == qid:
+                answer_dict = {}
+                for header in answer:
+                    answer_dict[header] = answer[header]
+                answers.append(answer_dict)
+        return answers
--- server.py
+++ server.py
@@ -4,11 +4,26 @@ import connection
 
 app = Flask(__name__)
 
+
+@app.route("/")
 @app.route('/list', methods=["GET","POST"])
 def route_list():
-    return render_template("list.html")
+    questions = data_handler.get_all_data(data_handler.QUESTION_PATH)
+    return render_template("list.html", questions=questions)
 
-    
+
+
+@app.route("/question/<question_id>", methods=["GET","POST"])
+def route_question(question_id):
+    question= data_handler.get_q_by_id(question_id)
+    answers = data_handler.get_answers_by_qid(question_id)
+    return render_template("question.html", question = question,answers=answers)
+
+@app.route("/add-question", methods=["GET", "POST"])
+def route_add_question():
+    return render_template("add-question.html")
+
+
 if __name__ == '__main__':
     app.run(
         port=5000,
